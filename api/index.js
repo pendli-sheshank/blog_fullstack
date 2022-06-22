69bbe1ac6830fd53014127b6974ea1aad7cc5693
sheshankp <sheshank3336@gmail.com>
2022-06-22 16:20:28 +0530

    REST API DONE

---
+++ api/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoriesRoute = require("./routes/categories");
+
+const multer = require("multer");
+
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGOOSE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to DB"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cd) => {
+    cd(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoriesRoute);
+
+app.listen("5000", () => {
+  console.log("Server is running");
+});
